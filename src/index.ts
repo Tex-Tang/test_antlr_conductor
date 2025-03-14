aea6d3afc6221312f950b4f0a4aedc92d6e48f4a
Tex-Tang <tex_tang@outlook.com>
2025-03-14 16:30:21 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { initialise } from "conductor/dist/conductor/runner/util/";
 import { SimpleLangEvaluator } from "./SimpleLangEvaluator";
 
-const {runnerPlugin, conduit} = initialise(SimpleLangEvaluator);
+const { runnerPlugin, conduit } = initialise(SimpleLangEvaluator);
